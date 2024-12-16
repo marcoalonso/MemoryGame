e17d280f7c8ab3e31cb4ca6f154923a6527b32be
Marco Alonso Rodriguez <marcoalonsorota@hotmail.com>
2024-12-16 11:50:06 -0600

    Sounds and vibration added

--- MemoryGame/MemoryGameViewModel.swift
+++ MemoryGame/MemoryGameViewModel.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 import SwiftUI
+import AVFoundation
+import UIKit
 
 class MemoryGameViewModel: ObservableObject {
     @Published var cards: [CardModel] = []
@@ -17,18 +19,19 @@ class MemoryGameViewModel: ObservableObject {
         "lion", "tiger", "elephant", "giraffe", "monkey",
         "zebra", "panda", "fox"
     ]
-    
+    private var audioPlayer: AVAudioPlayer?
+
     init() {
         setupGame()
     }
-    
+
     func setupGame() {
         score = 0
         flippedCards = []
         let shuffledImages = (animalImages + animalImages).shuffled()
         cards = shuffledImages.map { CardModel(imageName: $0) }
     }
-    
+
     func flipCard(_ card: CardModel) {
         guard let index = cards.firstIndex(where: { $0.id == card.id }),
               !cards[index].isFlipped,
@@ -41,7 +44,7 @@ class MemoryGameViewModel: ObservableObject {
             checkForMatch()
         }
     }
-    
+
     private func checkForMatch() {
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
             guard self.flippedCards.count == 2 else { return }
@@ -51,15 +54,19 @@ class MemoryGameViewModel: ObservableObject {
             
             if firstCard.imageName == secondCard.imageName {
                 self.markCardsAsMatched([firstCard, secondCard])
+                self.playSound(named: "match")
+                self.triggerHapticFeedback(type: .success)
                 self.score += 10
             } else {
                 self.unflipCards([firstCard, secondCard])
+                self.playSound(named: "no_match")
+                self.triggerHapticFeedback(type: .error)
                 self.score -= 2
             }
             self.flippedCards = []
         }
     }
-    
+
     private func markCardsAsMatched(_ cardsToMatch: [CardModel]) {
         for card in cardsToMatch {
             if let index = cards.firstIndex(where: { $0.id == card.id }) {
@@ -67,12 +74,27 @@ class MemoryGameViewModel: ObservableObject {
             }
         }
     }
-    
+
     private func unflipCards(_ cardsToUnflip: [CardModel]) {
         for card in cardsToUnflip {
             if let index = cards.firstIndex(where: { $0.id == card.id }) {
                 cards[index].isFlipped = false
             }
+        }
+    }
+
+    private func triggerHapticFeedback(type: UINotificationFeedbackGenerator.FeedbackType) {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(type)
+    }
+
+    private func playSound(named soundName: String) {
+        guard let soundURL = Bundle.main.url(forResource: soundName, withExtension: "mp3") else { return }
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
+            audioPlayer?.play()
+        } catch {
+            print("Error playing sound: \(error.localizedDescription)")
         }
     }
 }
