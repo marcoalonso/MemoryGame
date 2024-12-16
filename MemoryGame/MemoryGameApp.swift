fe6facfca3b12f7df3c55e7f6052fc69ca033d08
Marco Alonso Rodriguez <marcoalonsorota@hotmail.com>
2024-12-16 10:38:33 -0600

    Initial Commit

---
+++ MemoryGame/MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryGameApp.swift
+//  MemoryGame
+//
+//  Created by Marco Alonso Rodriguez on 16/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
